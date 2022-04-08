7d900f453711ee4566e8cd082c88289450eec2b6
Kurisu <>
2022-04-08 17:35:50 +0200

    restructuring modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,35 @@
 #[cfg(feature = "mocked")]
-mod mocks {
+pub mod mocked {
+    pub use crate::common::*;
+
+    pub(crate) mod ordered_plugin;
+    pub(crate) mod plugin_descriptor;
+
     mod app;
     mod plugin;
     pub use self::{app::MockApp as App, plugin::MockPlugin, plugin::Plugin};
+    pub use __mock_MockApp::__add_system::Expectation as add_system_Expectation;
 }
 
+#[cfg(not(feature = "mocked"))]
 pub mod release {
+    pub use crate::common::*;
 
-    mod ordered_plugin;
-    mod plugin_descriptor;
+    pub(crate) mod ordered_plugin;
+    pub(crate) mod plugin_descriptor;
 
-    pub use ordered_plugin::{app_dummy::AppDummy, OrderedPlugin, PlainDescriptor};
-    pub use plugin_descriptor::{PluginDescriptor, PluginDescriptorCoercion};
-
-    #[cfg(feature = "mocked")]
-    pub use crate::mocks::*;
-    use bevy::ecs::schedule::IntoSystemDescriptor;
-    #[cfg(not(feature = "mocked"))]
     pub use bevy::prelude::{App, Plugin};
+}
+
+mod common {
+    #[cfg(feature = "mocked")]
+    use crate::mocked as root;
+    #[cfg(not(feature = "mocked"))]
+    use crate::release as root;
+
+    pub(crate) use bevy::ecs::schedule::IntoSystemDescriptor;
+    pub use root::ordered_plugin::{app_dummy::AppDummy, OrderedPlugin, PlainDescriptor};
+    pub use root::plugin_descriptor::{PluginDescriptor, PluginDescriptorCoercion};
 }
 
 // unsafe impl Sync for TestPlugin {}
