759c19ee61a655096b51123fcaa7980090bc1ec3
Kurisu <>
2022-04-08 21:20:35 +0200

    now we have to figure out how to get sized SystemLabels...

--- src/inner/ordered_plugin/app_dummy.rs
+++ src/inner/ordered_plugin/app_dummy.rs
@@ -1,4 +1,10 @@
 use super::*;
+
+use bevy::{
+    ecs::schedule::{ParallelSystemDescriptor, SystemDescriptor},
+    prelude::{ParallelSystemDescriptorCoercion, SystemLabel},
+    reflect::TypeData,
+};
 pub struct AppDummy<'a> {
     app: &'a mut App,
     plugin_descriptor: &'a PluginDescriptor,
@@ -32,11 +38,44 @@ impl AppDummy<'_> {
         self
     }
 
+    fn f<T: SystemLabel + ?Sized>(
+        sys: ParallelSystemDescriptor,
+        label: Box<T>,
+    ) -> ParallelSystemDescriptor {
+        //   sys.label(*label)
+        todo!();
+    }
+
+    fn label_parallel_system(
+        &mut self,
+        mut system_descriptor: ParallelSystemDescriptor,
+    ) -> SystemDescriptor {
+        let label = self.plugin_descriptor.labels[0].clone();
+        let r = Self::f(system_descriptor, label);
+        // for x in &self.plugin_descriptor.labels {
+        //     //
+        //     system_descriptor = system_descriptor.label(*x);
+        // }
+        SystemDescriptor::Parallel(r)
+    }
+
+    fn add_system_impl<Params>(&mut self, system: impl IntoSystemDescriptor<Params>) -> &mut Self {
+        println!("run app_dummy::add_system_impl");
+
+        let system_descriptor = match system.into_descriptor() {
+            bevy::ecs::schedule::SystemDescriptor::Parallel(p) => {
+                Self::label_parallel_system(self, p)
+            }
+            bevy::ecs::schedule::SystemDescriptor::Exclusive(e) => todo!(),
+        };
+
+        self.app.add_system(system_descriptor);
+        self
+    }
+
     #[cfg(not(feature = "mocked"))]
     pub fn add_system<Params>(&mut self, system: impl IntoSystemDescriptor<Params>) -> &mut Self {
-        println!("run app_dummy::release_add_system");
-        self.app.add_system(system);
-        self
+        Self::add_system_impl(self, system)
     }
 
     #[cfg(feature = "mocked")]
@@ -44,8 +83,6 @@ impl AppDummy<'_> {
         &mut self,
         system: T,
     ) -> &mut Self {
-        println!("run app_dummy::mocked_add_system");
-        self.app.add_system(system);
-        self
+        Self::add_system_impl(self, system)
     }
 }
